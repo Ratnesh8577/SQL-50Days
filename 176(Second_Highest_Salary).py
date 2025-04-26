12d5986714d66a17bf3c173c6d1ee9ed4a1002ad
Ratnesh Chauhan <ratneshchauhan279@gmail.com>
2025-04-26 18:46:35 +0530

    Add files via upload

---
+++ 176(Second_Highest_Salary).py
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+"""
+
+SELECT max(salary) AS SecondHighestSalary
+FROM Employee 
+WHERE Salary <> (SELECT max(salary) 
+                 FROM Employee)
+                 
+                 """
